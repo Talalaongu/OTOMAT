d7eb25ad45f1e7a1651cca7da43a62d789cac8b9
Talalaongu <50053618+Talalaongu@users.noreply.github.com>
2019-04-27 23:10:08 +0700

    Add files via upload

---
+++ OTOMAT/OTOMAT/Aggregate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OTOMAT
+{
+    class Aggregate
+    {
+        private State _Element;
+        public Aggregate _Next;
+
+        public State Element { get => _Element; set => _Element = value; }
+
+        //public Aggregate()
+        //{
+        //    _Next = null;
+        //}
+
+        public bool IsMember(State q, Aggregate A)
+        {
+
+            return false;
+        }
+    }
+}
